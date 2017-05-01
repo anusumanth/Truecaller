977af75fb12e7c1fffdebe1a5508a7bb04963dc1
anusumanth <anupama.shashi@gmail.com>
2017-05-01 22:05:43 +0530

    Add files via upload

--- routes/caller.js
+++ routes/caller.js
@@ -1,19 +1,18 @@
-var connection = require('./dbConnection');
+
 exports.registerduser=function(req, res) {
    
- var data = {
-        "error":1,
-        "Books":""
-    };
-    
-    connection.query("SELECT * from Employee",function(err, rows, fields){
-        if(rows.length != 0){
-            data["error"] = 0;
-            data["Books"] = rows;
-            res.json(data);
-        }else{
-            data["Books"] = 'No books Found..';
-            res.json(data);
-        }
-    });
+
+var mdn= req.query.mdn;
+ console.log(mdn);
+ res.status(200).send('Hello');
+
+};
+
+exports.updateuser=function(req, res) {
+  
+ if (!req.body) return res.sendStatus(400);
+    console.log(JSON.stringify(req.body));
+    console.log(req.body);
+    res.status(200).send(req.body);
+
 };
