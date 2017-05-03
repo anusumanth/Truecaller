4574ee177436ef8fc980eb6f3bc4787efe8f9a1e
anusumanth <anupama.shashi@gmail.com>
2017-05-03 05:48:57 +0530

    Add files via upload

--- routes/caller.js
+++ routes/caller.js
@@ -1,13 +1,18 @@
 var connection = require('./dbConnection');
+
 exports.getuser=function(req, res) {
+	 var data = {
+        	"error":1,
+        	"Name":""
+    	};
 	var mdn= req.query.mdn;
 	connection.query("SELECT Name from Contacts where MDN = ?", mdn ,function(err, rows){
         if(rows.length != 0){
             data["error"] = 0;
-            data["Contacts"] = rows;
+            data["Name"] = rows[0].Name;
             res.status(200).json(data);
         }else{
-            data["Contacts"] = 'No ContactsSFound..';
+            data["Name"] = 'No Contacts Found..';
             res.status(200).json(data);
         }
     });
@@ -17,7 +22,11 @@ exports.getuser=function(req, res) {
 };
 
 exports.registerUser=function(req, res) {
-  
+   var data = {
+        "error":1,
+	"Message":"",
+        "UID":""
+    };
  if (!req.body) 
 	return res.sendStatus(400);
 
@@ -28,16 +37,25 @@ delete user.UID;
 connection.query('SELECT * from Users WHERE UID=?',uid,function(err,rows){
 	if(rows.length != 0){
          console.log('row present', rows[0].UID);
-         res.status(200).send(rows);
+	 data["error"] = 0;
+	data["Message"]="Registered User";
+	data["UID"] = rows[0].UID;	
+         res.status(200).json(data);
     }else{
-        connection.query('INSERT INTO Users SET ?', user, function(err,res){
+        connection.query('INSERT INTO Users SET ?', user, function(err,rows){
             if(err) {
-                var mes = {"Message":"MDN already present"};
+                data["error"]=1;
+		        data["Message"]="Duplicate MDN";
                 console.log("MDN already present");
-                console.log(mes);
-                res.status(200).send(mes);
+                console.log(data.error);
+                res.status(400).json(data);
             }
+	    data["error"] = 0;
+	data["Message"]="User registered successfully";
+	data["UID"] = res.insertId;	
+         
             console.log('Last insert ID:', res.insertId);
+	    res.status(200).json(data);
     });	
     }
 });
@@ -50,7 +68,21 @@ connection.query('SELECT * from Users WHERE UID=?',uid,function(err,rows){
 exports.syncContacts=function(req,res){
 if (!req.body) 
 	return res.sendStatus(400);
-
-var contacts = req.body.contacts;
-
+	var contact;
+	var contacts = req.body.contacts;
+	for(i=0;i<contacts.length;i++){
+		contact = contacts[i];
+		connection.query('INSERT INTO Contacts SET ?', contact , function(err){
+            if(err) {
+                throw err;
+               
+            }
+            console.log('Last insert ID:', res.insertId);
+	    
+        })
+ data["error"] = 0;
+	data["Message"]="Contacts Synced successfully";
+	data["UID"] =req.body.UID;
+	res.status(200).json(data);
+	}
 };
